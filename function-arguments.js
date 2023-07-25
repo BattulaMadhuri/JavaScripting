f322c8c23d8695dedad7aa96b617d9c6fbca9dfd
Madhuri Battula <137879232+BattulaMadhuri@users.noreply.github.com>
2023-07-25 15:04:48 +0530

    Add files via upload

---
+++ function-arguments.js
@@ -0,0 +1,11 @@
+function math (a,b,c){
+    a=a+b*c;
+    console.log(a);
+}
+math(53,61,67);
+
+// function math (a, b, c)
+// {
+//     return (b * c) + a;
+// }
+// console.log(math(53, 61, 67));
